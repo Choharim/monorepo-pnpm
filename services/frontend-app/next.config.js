06f187818d2d3e3f3ec8c7b9148b87e10b434896
Choharim <73021666+Choharim@users.noreply.github.com>
2023-06-11 19:15:38 +0900

    chore: monorepo root에 내부 패키지 shared-components,외부 패키지 next-transpile-modules 설치

--- services/frontend-app/next.config.js
+++ services/frontend-app/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const withTM = require("next-transpile-modules")([
+  "@monorepo/shared-components",
+]);
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
